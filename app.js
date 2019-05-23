ba6bb393026cb2b799a7cb627366487d3cb8ffc8
Oscar <oscarjavierho@gmail.com>
2019-05-23 11:55:42 -0430

    Heroku Fix

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.get('/', (req, res) => res.json({ msg: 'themisto' }));
 
 //Route
 app.post('/search', function(req, res) {
-
   // Get query on get.body
   const searchorder = {
     _id: req.body._id,
@@ -30,21 +29,23 @@ app.post('/search', function(req, res) {
       query: req.body.searchdata.query,
       provider: req.body.searchdata.provider,
       options: {user: req.body.searchdata.options.user, password: req.body.searchdata.options.password},
-      callbackurl: `https://ganymede2.herokuapp.com/api/product/search-order/${req.body._id}`
+      callbackurl: req.body.searchdata.callbackurl,
     },
-    orderstatus: "processing",
-    productresult: {}
+    orderstatus: "processing"
   };
 
   res.json(searchorder);
+  console.log(searchorder);
+  console.log('ahora puppeteer');
 
   search(searchorder);
 });
 
-const search = (req) => {
+
+const search = (req) => { 
   const query = req.searchdata.query;
   (async () => {
-    /* Initiate the Puppeteer browser */
+    // Initiate the Puppeteer browser 
     const browser = await puppeteer.launch({ 
       headless: true,
       'args' : [
@@ -54,54 +55,67 @@ const search = (req) => {
     });
 
     const page = await browser.newPage();
+
     /* Go to the IMDB Movie page and wait for it to load */
     await page.goto(
       `https://listado.mercadolibre.com.ve/${query}`,
       { waitUntil: 'networkidle2' });
 
-    var result = await page.evaluate(() => {
-      // Set the products
-      let productos=[];
-      // Find the products
-      const productsElms = document.querySelectorAll('li[class="results-item highlighted article stack item-without-installmets"]');
-      // Get every product
-      productsElms.forEach((productelement) => { 
-        let productJson = {};
-        try {
-          productJson.SKU = productelement.querySelector('div[class="rowItem item highlighted item--stack item--has-row-logo new "]').id,
-          productJson.name = productelement.querySelector('span[class="main-title"]').innerText,
-          productJson.price = productelement.querySelector('div[class="item__price "]').innerText,
-          productJson.description = productelement.querySelector('div[class="item__condition"]').innerText,
-          productJson.image = productelement.querySelector('img[class="lazy-load"]').src,
-          productJson.relatedsearchqueries = document.querySelector('ul[class="related-searches__list"]').innerText
-        } catch (exception){
-			console.log(exception);
-        }
-        productos.push(productJson);
-      });
+    const urls = await page.evaluate(() => Array.from(document.querySelectorAll('.item__info-title'), element => element.href));
 
-      return productos;
+    if (urls.length > 0 ) {
+      var data = [];
+      // for (let i = 0, total_urls = urls.length; i < total_urls; i++)
+      for (let i = 0; i < 2; i++) {
+        await page.goto(urls[i]);
 
-      });
+        // Get the data ...
+        let result = {};
+        result.SKU = await page.evaluate(() => document.querySelector('span[class="item-info__id-number"]').innerText );
+        result.title = await page.evaluate(() => document.querySelector('h1[class="item-title__primary "]').innerText);
+        result.description = await page.evaluate(() => document.querySelector('div[class="item-description__text"]').innerText);
+        result.price = await page.evaluate( () => document.querySelector('span[class="price-tag"]').innerText);
+        result.images = await page.evaluate( () =>  {
+          let images = [];
+          let size = document.querySelectorAll('.gallery__thumbnail').length;
+          if(size == 0 ) {
+            let image = document.querySelector('#gallery_dflt > div > figure > a > img').src;
+            images.push(image);
+          } else {
+            for (let i = 0; i < size; i++) {
+            let selector = `label[for="thumbgallery_default-${i}"] > img`;
+            let image = document.querySelector(selector).src;
+            images.push(image);
+            }
+          }
+        return images;
+        }); //Close images result
+      
+        data.push(result);
+      } //Close result bucle when done
+    } else {
+      let data = {}
+    }
+
+    console.log(data);
 
     //Object with the result
     let send = {
       _id: req._id,
       searchdata: req.searchdata,
       orderstatus: "fullfilled",
-      productresult: result
+      productresult: data
     }
-    
-  
+
     //Send the result
     axios
-      .post('https://ganymede2.herokuapp.com/api/product/save-search', send)
-      .then(res => console.log('axios send'))
+      .post(req.searchdata.callbackurl, send)
+      .then(res => console.log('axios send', req.searchdata.callbackurl))
       .catch(err => console.log(err));
 
     await browser.close();
-    
-  })();
-}
 
+  })(); //Close async
+}// Close Search
   
+    
